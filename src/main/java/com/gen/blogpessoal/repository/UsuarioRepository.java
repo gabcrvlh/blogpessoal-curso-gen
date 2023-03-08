bd01f3da9f1d84efc1b621e99c9402e64f3f25a8
Gabriela <Gabriela@MacBook-Pro-de-Gabriela.local>
2023-03-08 15:15:29 -0300

    teste unitário

--- src/main/java/com/gen/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/gen/blogpessoal/repository/UsuarioRepository.java
@@ -2,14 +2,19 @@ package com.gen.blogpessoal.repository;
 
 import com.gen.blogpessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    Optional<Usuario> findByUsuario(String usuario);
+    public Optional<Usuario> findByUsuario(String usuario);
+
+    List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
 
 
 }
